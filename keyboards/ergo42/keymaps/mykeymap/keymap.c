904ff3a82086be1839e4fc505efe5281c2f5695d
inamuu <kzm0211@gmail.com>
2018-06-23 14:28:30 +0900

    fix typo

--- keyboards/ergo42/keymaps/mykeymap/keymap.c
+++ keyboards/ergo42/keymaps/mykeymap/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_GRV,  KC_BSLS,  KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_EQL,  KC_MINS,  KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_LBRC, KC_RBRC,  KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_ESC,  KC_LTCL, KC_LALT, KC_LGUI, KC_MHEN, MO(META), KC_SPC,  KC_ENT,   MO(SYMB), KC_HENK, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+    KC_ESC,  KC_LCTL, KC_LALT, KC_LGUI, KC_MHEN, MO(META), KC_SPC,  KC_ENT,   MO(SYMB), KC_HENK, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
   ),
 
   /* META
